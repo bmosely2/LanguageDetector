3ad3b910bded4e5b876b6998789e05d4157341c5
breinmosely <bmosely2@illinois.edu>
2019-04-25 16:27:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LanguageDetector.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
+public class LanguageDetector {
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+import com.example.myapplication.LanguageDetector;
+public class MainActivity {
+
+
+}
