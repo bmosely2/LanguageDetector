05927666be48bbc5b6095b1cc75861d249d9a61f
breinmosely <bmosely2@illinois.edu>
2019-04-25 17:06:53 -0500

    Main

--- app/src/main/java/com/example/myapplication/LanguageDetector.java
+++ app/src/main/java/com/example/myapplication/LanguageDetector.java
@@ -4,5 +4,15 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 
 public class LanguageDetector {
-
+    public static String getLanguage (final String json) {
+        JsonParser parser = new JsonParser() ;
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        if (result.get("success").equals(true)) {
+            JsonArray array = result.get("results").getAsJsonArray();
+            JsonObject code = array.get(0).getAsJsonObject();
+            String languageCode = code.get("language_code").getAsString();
+            return languageCode;
+        }
+        return null;
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.myapplication;
-import com.example.myapplication.LanguageDetector;
-public class MainActivity {
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
 }
